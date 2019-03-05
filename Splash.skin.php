c4fe3c2d5daab38fe9cc696e75925efa0a3fe4aa
Jack Phoenix <ashley@uncyclomedia.co>
2019-03-05 02:25:16 +0200

    Don't use OutputPage#addModuleScripts for loading a CSS-only module that is already loaded w/ addModuleStyles()
    OutputPage#addModuleScripts is being removed from core in MW 1.33 (see https://gerrit.wikimedia.org/r/494063/) and it was formally deprecated in MW 1.31.
    
    Moved the addModuleStyles() call to its proper place while at it and tweaked the documentation + bumped skin version number.
    
    Change-Id: Ifdcace845be266295fe54c10c9367206e1bffdc5

--- Splash.skin.php
+++ Splash.skin.php
@@ -9,26 +9,29 @@ class SkinSplash extends SkinTemplate {
 		$template = 'SplashTemplate';
 
 	/**
+	 * Set a necessary <meta> tag.
+	 * This is also the place where we'd load JS if we had any custom JS, but we
+	 * don't.
+	 *
 	 * @param OutputPage $out
 	 */
 	public function initPage( OutputPage $out ) {
 		parent::initPage( $out );
 
 		$out->addMeta( 'viewport', 'width=device-width, initial-scale=1, maximum-scale=1' );
-
-		# Add css/js
-		$out->addModuleStyles( [
-			'mediawiki.skinning.content.externallinks',
-			'skins.splash'
-		] );
-		$out->addModuleScripts( 'skins.splash' );
 	}
 
 	/**
 	 * Add CSS via ResourceLoader
+	 *
 	 * @param $out OutputPage
 	 */
 	function setupSkinUserCss( OutputPage $out ) {
 		parent::setupSkinUserCss( $out );
+
+		$out->addModuleStyles( [
+			'mediawiki.skinning.content.externallinks',
+			'skins.splash'
+		] );
 	}
 }
